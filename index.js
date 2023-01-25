1971bb9da87a53bb41df3f610c205446f216a394
Umera.Z <cryptokukduku@gmail.com>
2023-01-25 15:07:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// function popup() {
+//     alert("Hello it is a popup notification")
+// }
+// const popup = () => {
+//     alert("Hello it is a popup notification")
+// }
+ 
+// let x = 57;
+// if(x>10){
+//     console.log("Number is greater")
+// } else{
+//     console.log("Number is Smaller")
+// }
+
+
+// let a = 50;
+// let b = 60;
+// let c = 80;
+
+// if(a>b){
+//     console.log("a is greater")
+// }else if(b>c){
+//     console.log("b is greater")
+// }else{
+//     console.log("c is greater")
+// }
+
+// let a = 50;
+// let c = 80;
+// a==c ? console.log("greater") : console.log("smaller")
+
+// let x;
+// switch(x = new Date().getDay()){
+//         case 0:
+//         x = "Sunday";
+//         break
+//         case 1:
+//             x = "Monday";
+//             break
+//             case 2:
+//         x = "Tuesday";
+//         break
+//         case 3:
+//         x = "Wednesday";
+//         break
+//         case 4:
+//         x = "Thursday";
+//         break
+//         case 5:
+//         x = "Friday";
+//         break
+//         case 6:
+//         x = "Saturday";
+// }
+// document.getElementById("demo").innerHTML = x 
+// let x = 50
+// let a = x>10; 
+// let b = x<10; 
+// if(a&&b){
+//     console.log("Hello");
+// } else{
+//     console.log("World")
+// }
+
+
+// function add(a,b){
+//     return a*2 + b*5;
+// }
+
+// console.log(add(5,10))
+
+
+// function myFunction() {
+//     document.getElementById("id2").setAttribute("class","democlass");
+//     alert("color is changed")
+// }
+// myFyunction()
+
+
+
+
+
+
+
+
+
+
+
+
